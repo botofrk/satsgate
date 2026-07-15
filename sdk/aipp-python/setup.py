1729ccb15f3350adcb7508ee0221a6b0921d4cbd
aippde <aippde@gmail.com>
2026-07-15 21:27:52 +0300

    chore: bump SDK versions to 1.2.2 for npm/PyPI publish

--- sdk/aipp-python/setup.py
+++ sdk/aipp-python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aipp-sdk",
-    version="1.2.1",
+    version="1.2.2",
     description="Official Python SDK for AIPP - The Lightning Network Split-Payment Gateway",
     long_description=open("README.md", "r", encoding="utf-8").read() if open("README.md", "r", encoding="utf-8") else "",
     long_description_content_type="text/markdown",
